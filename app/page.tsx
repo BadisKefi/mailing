82456a1db3fc7a6eda6045c1ad110150b81e6b17
badiskefi <badiskefi@gmail.com>
2023-11-02 16:19:10 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Contact from "@/components/contact";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Contact />
+    </main>
+  );
+}
